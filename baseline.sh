16fbf16946a533953e1c954b4982c96a3f095b88
ngundotra <noah@gundotra.org>
2017-11-14 11:16:57 -0800

    lower imsize to 12

--- baseline.sh
+++ baseline.sh
@@ -3,18 +3,15 @@
 
 for seed in 32 1729 943 6543
 do
-	for imsize in {5..20}
+	for imsize in 5 7 9 10 11 12
 	do
-		for wallp in $(seq 0.0 0.1 0.7)
+		for rewardp in 0.0 0.3 0.5 0.7
 		do
-			for rewardp in $(seq 0.0 0.1 0.8)
+			for agent in 'optimal' 'naive' 'sophisticated' 'myopic'
 			do
-				for agent in 'optimal' 'naive' 'sophisticated' 'myopic'
-				do
-				  	echo "beginning: imsize-$imsize wall_prob-$wallp reward_prob-$rewardp agent-$agent seed-$seed"
-				  	run_script="$(time python gridworld_data.py --imsize $imsize --wall_prob $wallp --reward_prob $rewardp --agent $agent --seed $seed)"
-				  	echo "${run_script}"
-				done
+			  	echo "beginning: imsize-$imsize reward_prob-$rewardp agent-$agent seed-$seed"
+			  	run_script="$(time python gridworld_data.py --imsize $imsize --reward_prob $rewardp --agent $agent --seed $seed)"
+			  	echo "${run_script}"
 			done
 		done
 	done
